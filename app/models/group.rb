af645439bdfe1552383812ca654e9cffd56e4d58
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-07 15:50:31 +0900

    defined the instance method to show the latest message in the sidebar

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
   validates :name, presence: true
+
+  def show_latest_message(user_group)
+    if user_group.messages.present?
+      if user_group.messages.last.image.present?
+        "画像が投稿されました"
+      else
+        user_group.messages.last.body
+      end
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end
