3972326f7368d11fe524db8be45d7fe01ef32795
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-07 16:58:39 +0900

    changed the code to changed the order of groups shown in the sidebar
    最後のメッセージ投稿が最新の順に表示されるようにした。

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   def show_latest_message(user_group)
-    if (last_message = user_group.messages.last).present?
+    if (last_message = user_group.messages.first).present?
       last_message.body? ? last_message.body : "画像が投稿されました"
     else
       "まだメッセージはありません。"
