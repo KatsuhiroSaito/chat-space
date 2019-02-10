3186ff33f9642d30cc3f7b820b3d9757a0234350
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-10 15:05:01 +0900

    added the code

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe 'GET #index' do
+    context "logged in" do
+      before do
+        login_user user
+        get :index, params: { group_id: group.id }
+      end
+
+    end
+
+    context "not logged in" do
+    end
+  end
 end
