d588981306d8fb6b8aea34e2ddf9acc337a5cc9b
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-10 22:17:48 +0900

    included Devise::Test::ControllerHelpers instead of Devise::TestHelpers and deleted unnecessary part

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'devise'
-require File.expand_path("spec/support/controller_macros.rb")
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -35,8 +34,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
