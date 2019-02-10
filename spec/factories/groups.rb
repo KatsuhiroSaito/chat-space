87da0fb1f67a3defcc15f7936ef0c49aa8f13e09
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-10 21:51:01 +0900

    edited the factory files

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :group do
     name Faker::Team.name
+    created_at { Faker::Time.between(2.weeks.ago, 3.days.ago, :all) }
+    updated_at { Faker::Time.between(2.days.ago, Date.today, :all) }
   end
 end
