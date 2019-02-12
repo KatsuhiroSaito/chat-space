02969f3baf2434082065503f795692f8fd296172
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-13 00:08:40 +0900

    submitイベントが発火したときにAjaxを使用して、messages#createが動くようにした

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,5 +1,17 @@
 $(function(){
-  $('#new_message').on('submit', function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var url = $(this).attr('action')
+    var formData = new FormData(this);
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
 
   });
 })
