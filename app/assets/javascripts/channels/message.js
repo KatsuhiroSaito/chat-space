c11d5b7b906d77fe501577a7c946cd5476e06642
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-14 17:22:54 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを作成し、画面の一番下に追加する

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,8 +1,31 @@
 $(function(){
+  function buildHTML(message){
+
+    var img = "";
+    if (message.image["url"] !== null) {
+        img = `<img class="message__lower-image" src="${message.image.url}">`
+    }
+    var html = `<div class="message__upper">
+                  <div class="message__upper-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="message__upper-time">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="message__lower">
+                  <div class="message__lower-body">
+                    ${message.body}
+                  </div>
+                  ${ img }
+                </div>`
+    return html;
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var url = $(this).attr('action')
     var formData = new FormData(this);
+    var url = $(this).attr('action')
 
     $.ajax({
       url: url,
@@ -13,5 +36,10 @@ $(function(){
       contentType: false
     })
 
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html)
+      $('.chat-main__footer-form-text').val('')
+    })
   });
-})
+});
