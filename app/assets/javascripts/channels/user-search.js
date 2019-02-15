b60374088ac0aeacd5fa5cb0b57781d9007551b0
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-16 00:51:28 +0900

    変数htmlを定義

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -1,5 +1,9 @@
 $(function(){
   function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">ユーザー名</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
+                </div>`
   }
 
   $("#user-search-field").on("keyup", function() {
