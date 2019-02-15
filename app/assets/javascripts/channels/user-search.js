fcefa3289d1f612da1ca130e865ed81c18c196cf
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-16 01:04:53 +0900

    関数appendUser内に変数htmlをHTMLに追加するための記述を追加

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -6,6 +6,7 @@ $(function(){
                   <p class="chat-group-user__name">ユーザー名</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
                 </div>`
+    search_list.append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
