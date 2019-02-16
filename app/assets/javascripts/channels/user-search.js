c895da559ccff93b29ee731f2c98bbae842559b9
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-16 23:42:06 +0900

    関数addedUser内に変数htmlを定義

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -21,7 +21,11 @@ $(function(){
   }
 
   function addedUser() {
-
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
+                  <p class='chat-group-user__name'>${ user.name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
   }
 
   $("#user-search-field").on("keyup", function() {
