e158ef417de239d2b35b2508ab0d5546ddd8c5db
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-16 23:04:11 +0900

    変数名を"search_list"から"user_search_result"に変更

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -1,23 +1,23 @@
 $(function(){
-  var search_list = $("#user-search-result");
+  var user_search_result = $("#user-search-result");
 
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`
-    search_list.append(html);
+    user_search_result.append(html);
   }
 
   function appendNoUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user }</p>
                 </div>`
-    search_list.append(html);
+    user_search_result.append(html);
   }
 
   function emptySearchList() {
-    search_list.empty();
+    user_search_result.empty();
   }
 
   $("#user-search-field").on("keyup", function() {
