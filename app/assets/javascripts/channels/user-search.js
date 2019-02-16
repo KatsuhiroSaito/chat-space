8a64667426b248fff945e5e6d0d142a23d775784
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-16 23:54:34 +0900

    変数user_name、user_idを定義

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -62,5 +62,7 @@ $(function(){
 
   $(document).on("click", ".chat-group-user__btn--add", function(){
     $(this).parent().remove();
+    user_name = $(this).attr("data-user-name");
+    user_id = $(this).attr("data-user-id");
   });
 });
