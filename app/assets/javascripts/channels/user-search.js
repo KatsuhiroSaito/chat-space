ca23f2dac0af75f75158cbfaa6d1bc5b4ab8f4f6
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-16 23:55:16 +0900

    関数addedUser内の変数htmlを修正

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -22,8 +22,8 @@ $(function(){
 
   function addedUser() {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
-                  <p class='chat-group-user__name'>${ user.name }</p>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
 
