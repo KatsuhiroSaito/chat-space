19334b64d828601b147cc7fe841021262a5d3f49
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-17 20:45:18 +0900

    関数appendUserの名前をappendMatchedUserに変更

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -1,7 +1,7 @@
 $(function(){
   var user_search_result = $("#user-search-result");
 
-  function appendUser(user) {
+  function appendMatchedUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
@@ -48,7 +48,7 @@ $(function(){
       if (users.length !== 0) {
         users.forEach(function(user){
           if ($.inArray(user.id.toString(), alreadyAddedMemberIds) == -1) {
-            appendUser(user);
+            appendMatchedUser(user);
           }
         });
       }
