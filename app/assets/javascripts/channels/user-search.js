b17aac835903bb2fcdce22397a40d1225da4d32f
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-17 17:36:03 +0900

    inputタグにchat-group-user-inputというクラスを追加

--- app/assets/javascripts/channels/user-search.js
+++ app/assets/javascripts/channels/user-search.js
@@ -22,7 +22,7 @@ $(function(){
 
   function addUser(user_name, user_id) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }' class='chat-group-user-input'>
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
