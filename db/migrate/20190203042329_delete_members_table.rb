a24281944f8b3805c60aa78e83bf5ebade2ea6eb
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-02-20 22:56:15 +0900

    comment out

--- db/migrate/20190203042329_delete_members_table.rb
+++ db/migrate/20190203042329_delete_members_table.rb
@@ -1,5 +1,5 @@
-class DeleteMembersTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :members
-  end
-end
+# class DeleteMembersTable < ActiveRecord::Migration[5.0]
+#   def change
+#     drop_table :members
+#   end
+# end
